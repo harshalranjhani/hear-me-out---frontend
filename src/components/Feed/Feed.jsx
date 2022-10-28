ca1066b70262ea81c5a6be4e54c0955ee0c5180c
harshalranjhani <ranjhaniharshal@gmail.com>
2022-10-28 12:37:09 +0530

    add info on feed page

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -6,6 +6,12 @@ function Feed(props) {
     <div>
       {/* <Typography vairant="h1">Welcome to feed.</Typography> */}
       <h1>Welcome to feed.</h1>
+      <p>
+        This is where you will see what you're followers have been listening to
+        recently. You can add comments and share your experiences.
+      </p>
+
+      <h4>Coming soon...</h4>
     </div>
   );
 }
